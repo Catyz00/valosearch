83da421d066c73ac27a768f7cf1bace4d5f0228d
Catarina Dalsan <catarina.dalsan.ribeiro@gmail.com>
2025-01-29 23:19:54 -0300

    nao funcionou nada

--- valosearch/src/App.jsx
+++ valosearch/src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Importe o arquivo CSS aqui
+import Header from './components/header';
+import SearchSection from './components/searchsection';
+import FunctionsSection from './components/functionsection';
+import AgentSection from './components/agentsection';
 
-import styles from "../src/styles/styles.css";
-import Header from "./components/header";
-import SearchSection from "./components/searchsection";
-import FunctionsSection from "./components/functionsection";
-import AgentSection from "./components/agentsection";
-
+// Defina o componente App
 const App = () => {
   return (
     <div>
       <p>Oiee</p>
-      <Header />
+      <Header className="bg-blue-500 text-white p-4" />
       <main>
         <SearchSection />
         <FunctionsSection />
@@ -19,5 +20,13 @@ const App = () => {
     </div>
   );
 };
+
+// Renderize o componente App
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 export default App;
---
+++ valosearch/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Ajuste conforme necessário
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
