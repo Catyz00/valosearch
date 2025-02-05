7207f16f3d5a4644981fca94be865c35a4888a86
Catarina Dalsan <catarina.dalsan.ribeiro@gmail.com>
2025-02-05 11:09:04 -0300

    Update App.jsx

--- valosearch/src/App.jsx
+++ valosearch/src/App.jsx
@@ -4,14 +4,14 @@ import './index.css'; // Importe o arquivo CSS aqui
 import Header from './components/header';
 import SearchSection from './components/searchsection';
 import FunctionsSection from './components/functionsection';
-import AgentSection from './components/agentsection';
+import AgentSection from './components/Agentsection';
 
 // Defina o componente App
 const App = () => {
   return (
     <div>
       <p>Oiee</p>
-      <Header className="bg-blue-500 text-white p-4" />
+      <Header className="bg-blue-500 text-white p-4"/>
       <main>
         <SearchSection />
         <FunctionsSection />
